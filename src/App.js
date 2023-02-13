d7ad798f0eb1baea594d2a66b61307b4f9ed86e0
kiran010106 <84674193+kiran010106@users.noreply.github.com>
2023-02-13 16:55:23 +0530

    udated app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-// import Count from "./components/Count";
-// import FocusInput from "./components/FocusInput";
-// import Render from "./components/Render";
-// import State from "./components/State";
+import Count from "./components/Count";
+import FocusInput from "./components/FocusInput";
+import Render from "./components/Render";
+import State from "./components/State";
 import Track from "./components/Track";
 function App() {
   return (
     <div className="App">
-      {/* <State /> */}
-      {/* <FocusInput /> */}
-      {/* <Render /> */}
-      {/* <Count /> */}
+      { <State /> }
+      { <FocusInput /> }
+      { <Render /> }
+      { <Count /> }
       <Track />
     </div>
   );
